0ef1d1e893fb5c97df15acd7a567caa2a73522fc
Piotr <piotr.liszka.pl@gmail.com>
2021-03-14 11:09:33 +0100

    Step 38/105: Add fragment_voter_info.xml

--- app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoViewModel.kt
@@ -1,17 +1,31 @@
 package com.example.android.politicalpreparedness.election
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.politicalpreparedness.database.ElectionDao
+import com.example.android.politicalpreparedness.network.models.Election
 
 class VoterInfoViewModel(private val dataSource: ElectionDao) : ViewModel() {
 
-    //TODO: Add live data to hold voter info
+    //Done: Add live data to hold voter info
+    private val _election = MutableLiveData<Election>()
+    val election: LiveData<Election>
+        get() = _election
+    private val _isFollowed = MutableLiveData<Boolean>(false)
+    val isFollowed: LiveData<Boolean>
+        get() = _isFollowed
 
     //TODO: Add var and methods to populate voter info
 
     //TODO: Add var and methods to support loading URLs
 
     //TODO: Add var and methods to save and remove elections to local database
+    fun followElection() {
+        _isFollowed.value?.let { isFollow ->
+            _isFollowed.value = !isFollow
+        }
+    }
     //TODO: cont'd -- Populate initial state of save button to reflect proper action based on election saved status
 
     /**
