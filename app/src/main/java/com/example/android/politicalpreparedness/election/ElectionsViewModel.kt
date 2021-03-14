779c89bc86b595dcaecbfb07c0b08ed70109570f
Piotr <piotr.liszka.pl@gmail.com>
2021-03-14 08:25:09 +0100

    Step 30/105: Add fragment_election.xml

--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsViewModel.kt
@@ -1,13 +1,23 @@
 package com.example.android.politicalpreparedness.election
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.politicalpreparedness.network.models.Election
 
 //TODO: Construct ViewModel and provide election datasource
 class ElectionsViewModel: ViewModel() {
 
-    //TODO: Create live data val for upcoming elections
+    //Done: Create live data val for upcoming elections
+    private val _electionList = MutableLiveData<List<Election>>()
+    val electionList: LiveData<List<Election>>
+        get() = _electionList
 
-    //TODO: Create live data val for saved elections
+    //Done: Create live data val for saved elections
+    private val _savedElectionList = MutableLiveData<List<Election>>()
+    val savedElectionList: LiveData<List<Election>>
+        get() = _savedElectionList
+
 
     //TODO: Create val and functions to populate live data for upcoming elections from the API and saved elections from local database
 
